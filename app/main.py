992f7f6cf02949d66a0c55c20518edbb528d5d81
Jish2 <j.j.jappiecraft@gmail.com>
2024-06-15 15:36:40 -0700

    add exit 0

--- app/main.py
+++ app/main.py
@@ -7,14 +7,31 @@ def prompt():
     command = input()
     return command
 
+def get_commands():
+    return {
+        "exit": exit   
+    }
+
+def exit(num):
+    code = num[0]
+    # check if string is number
+    if not code.isdigit():
+        print(f"exit: {code}: numeric argument required")
+        sys.exit(255)
+    
+    sys.exit(int(code))
+
 def main():
     # Wait for user input
-    commands = []
+    commands = get_commands()
     command = prompt()
 
     while command:
-        if command not in commands:
-            print(f"{command}: command not found")
+        args = command.split(" ")
+        if args[0] not in commands:
+            print(f"{args[0]}: command not found")
+        else:
+            commands[args[0]](args[1:])
         command = prompt() 
 
 if __name__ == "__main__":
