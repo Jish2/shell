53efab0f82e820578619c317bac583ffe4454697
Jish2 <j.j.jappiecraft@gmail.com>
2024-06-15 15:27:20 -0700

    add repl (loop)

--- app/main.py
+++ app/main.py
@@ -1,20 +1,21 @@
 import sys
 
-
-def main():
-    # Uncomment this block to pass the first stage
+def prompt():
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
-    # Wait for user input
     command = input()
+    return command
 
+def main():
+    # Wait for user input
     commands = []
+    command = prompt()
 
-    if command not in commands:
-        print(f"{command}: command not found")
-        sys.exit(1)
-
+    while command:
+        if command not in commands:
+            print(f"{command}: command not found")
+        command = prompt() 
 
 if __name__ == "__main__":
     main()
